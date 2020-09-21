451f0d5069fbbd9c1c1460a4404d2cbcf03aa4c2
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-09-21 15:56:19 -0500

    removed unnecessary line from CreateTrack.css

---
+++ src/components/track/TrackList.js
@@ -0,0 +1,12 @@
+import React, {useState, useEfect} from 'react'
+
+const TrackList = props => {
+  const [tracks, ]
+  return(
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default TrackList
